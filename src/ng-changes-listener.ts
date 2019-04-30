ae7e2fa181483dacce9b0c5400309b9c6473dcfd
alfredo.salzillo <noctisbels@gmai.com>
2019-04-30 22:33:54 +0200

    feat(pencil): add support for single parameters and input feat(pencil): add ListenerData support feat(pencil): if multi-parameters in function listener non ListenerData will be all array of current value or current value if non array input is passed

--- src/ng-changes-listener.ts
+++ src/ng-changes-listener.ts
@@ -1,73 +1,83 @@
-import { SimpleChanges, EventEmitter } from '@angular/core';
+import { SimpleChanges, EventEmitter } from '@angular/core'
+import 'reflect-metadata'
 
-const isEventEmitter = <T = any>(value: any): value is EventEmitter<T> => 'emit' in value;
+const isEventEmitter = <T = any>(value: any): value is EventEmitter<T> => 'emit' in value
 
-const ChangesListenersSymbol = Symbol('ChangesListeners');
+const changesListenersKey = Symbol('changes-listeners')
+const getChangesListeners = (target: Object) =>
+  Reflect.getMetadata(changesListenersKey, target) as Function[]
+const defineChangesListeners = (target: Object) =>
+  Reflect.defineMetadata(changesListenersKey, [], target)
+const pushChangesListeners = (target: Object, value: Function) =>
+  getChangesListeners(target).push(value)
 
-export type ChangeListenerFn = (this: any, change: SimpleChanges) => void;
-export type ChangesPredicate = (changes: SimpleChanges) => boolean;
-export type ChangesListenerHost<T = any, S = keyof T> = {
-  ngOnChanges?(changes: SimpleChanges): void,
-  [ChangesListenersSymbol]: ChangeListenerFn[],
-  [P: string]: any,
-};
-const isChangesListenerHost = (value: any) : value is ChangesListenerHost => (
-  'ngOnChanges' in value
-  && ChangesListenersSymbol in value
-);
+export type ChangeListenerFn = (this: Object, change: SimpleChanges) => void
+export type ChangesPredicate = (changes: SimpleChanges) => boolean
 
-const ngOnChanges = (ngOnChanges: Function) => function (
-  this: ChangesListenerHost, changes: SimpleChanges,
-) {
-  if (ngOnChanges) ngOnChanges.call(this, changes);
-  this[ChangesListenersSymbol].forEach(listener => listener.call(this, changes));
-};
-const action = (
-  operation: (host: ChangesListenerHost, changes: SimpleChanges) => void,
-) => function (
-  this: ChangesListenerHost, changes: SimpleChanges,
-) { operation(this, changes); };
+export class ListenerData {
+  constructor(public changes: SimpleChanges) {}
+}
+
+const ngOnChanges = function(this: Object, changes: SimpleChanges) {
+  getChangesListeners(this).forEach((listener: Function) => listener.call(this, changes))
+}
+const action = (operation: (host: Object, changes: SimpleChanges) => void): ChangeListenerFn =>
+  function(this: object, changes: SimpleChanges) {
+    operation(this, changes)
+  }
 
 export const ChangesListener = (
   isChanged: ChangesPredicate,
-  transform: (changes: SimpleChanges) => any,
-): PropertyDecorator => (
-  target: any,
-  propertyKey: string | symbol,
-) => {
-  if (!isChangesListenerHost(target)) {
-    Object.defineProperty(target, ChangesListenersSymbol, {
-      value: [],
-    });
+  transform: (changes: SimpleChanges) => any
+): PropertyDecorator => (target: any, propertyKey: string | symbol) => {
+  if (!Reflect.hasOwnMetadata(changesListenersKey, target)) {
+    defineChangesListeners(target)
+    if ('ngOnChanges' in target) {
+      pushChangesListeners(target, target.ngOnChanges)
+    }
     Object.defineProperty(target, 'ngOnChanges', {
-      value: ngOnChanges(target.ngOnChanges),
-    });
+      value: ngOnChanges
+    })
   }
-  target[ChangesListenersSymbol].push(action(((host, changes) => {
-    if (typeof propertyKey !== 'string') {
-      // TODO lanch exception or FIX
-      return;
-    }
-    const listener = host[propertyKey];
-    if (!isChanged(changes)) {
-      return;
-    }
-    if (isEventEmitter(listener)) {
-      return listener.emit(transform(changes));
-    }
-    if (typeof listener === 'function') {
-      return listener.call(host, transform(changes));
-    }
-  })));
-};
+  const Type = Reflect.getMetadata('design:type', target, propertyKey)
+  pushChangesListeners(
+    target,
+    action((host, changes) => {
+      const listener = Reflect.get(host, propertyKey)
+      if (!isChanged(changes)) {
+        return
+      }
+      // cannot use only Type === EventEmitter for a know bug
+      // https://github.com/Microsoft/TypeScript/issues/18995
+      if (Type === EventEmitter || isEventEmitter(listener)) {
+        return listener.emit(transform(changes))
+      }
+      if (Type === Function) {
+        const parametersType: any[] = Reflect.getMetadata('design:paramtypes', target, propertyKey)
+        return listener.call(
+          host,
+          ...parametersType.map(paramType => {
+            if (paramType === ListenerData) {
+              return new ListenerData(changes)
+            }
+            return transform(changes)
+          })
+        )
+      }
+    })
+  )
+}
 
-const flatten = <T>(arr: T[] | T) => ([] as T[]).concat(arr);
-const currentValues = <C, S = keyof C>(inputs: S[]) => (changes: SimpleChanges) => inputs
-  .map(input => typeof input === 'string' && changes[input] && changes[input].currentValue);
+const flatten = <T>(arr: T[] | T) => ([] as T[]).concat(arr)
 const everyChanged = <C, S = keyof C>(inputs: S[]) => (changes: SimpleChanges) =>
-  inputs.every(input => typeof input === 'string' && input in changes);
+  inputs.every(input => typeof input === 'string' && input in changes)
+const currentValues = <C, S = keyof C>(inputs: S[]) => (changes: SimpleChanges) =>
+  inputs.map(input => typeof input === 'string' && changes[input] && changes[input].currentValue)
+const singleValue = <C, S = keyof C>(input: S) => (changes: SimpleChanges) =>
+  typeof input === 'string' && changes[input] && changes[input].currentValue
 
-export const ChangeListener = <C, S = keyof C>(inputs: S | S[]) => ChangesListener(
-  everyChanged<C, S>(flatten<S>(inputs)),
-  currentValues<C, S>(flatten<S>(inputs)),
-);
+export const ChangeListener = <C, S = keyof C>(inputs: S | S[]) =>
+  ChangesListener(
+    everyChanged<C, S>(flatten<S>(inputs)),
+    typeof inputs === 'string' ? singleValue(inputs) : currentValues<C, S>(flatten<S>(inputs))
+  )
--- test/ng-changes-listener.test.ts
+++ test/ng-changes-listener.test.ts
@@ -1,109 +1,141 @@
-import { EventEmitter, SimpleChange, SimpleChanges } from '@angular/core';
-import { ChangeListener } from '../src/ng-changes-listener';
-import { tap } from 'rxjs/operators';
+import { EventEmitter, SimpleChange, SimpleChanges } from '@angular/core'
+import { ChangeListener, ListenerData } from '../src/ng-changes-listener'
+import { tap } from 'rxjs/operators'
 
 class Test {
-  props1 = 0;
-  props2 = 0;
-  props3 = 0;
-  props4 = 0;
+  props1 = 0
+  props2 = 0
+  props3 = 0
+  props4 = 0
+  props5 = 0
+  changes: any = null
 
   constructor() {
     this.testProps1And2EmitterChanged
       .pipe(
         tap(([props3, props4]) => {
-          this.props3 = props3;
-          this.props4 = props4;
-        }),
+          this.props3 = props3
+          this.props4 = props4
+        })
       )
-      .subscribe();
+      .subscribe()
   }
 
   @ChangeListener(['props1'])
   testProps1Changed([props1]: number[]) {
-    this.props1 = props1;
+    this.props1 = props1
+  }
+
+  @ChangeListener('props5')
+  testProps5SingleChanged(props5: number) {
+    this.props5 = props5
   }
 
   @ChangeListener(['props1', 'props2'])
-  testProps1And2Changed([props1, props2]: number[]) {
-    this.props1 = props1;
-    this.props2 = props2;
+  testProps1And2Changed([props1, props2]: number[], data: ListenerData) {
+    this.props1 = props1
+    this.props2 = props2
+    this.changes = data.changes
   }
 
   @ChangeListener(['props3', 'props4'])
-  testProps1And2EmitterChanged = new EventEmitter<[number, number]>();
+  testProps1And2EmitterChanged = new EventEmitter<[number, number]>()
 }
 
 class TestWithNgOnChanges {
-  props0 = 0;
-  props1 = 0;
+  props0 = 0
+  props1 = 0
 
   ngOnChanges() {
-    this.props0 = 1;
+    this.props0 = 1
   }
 
   @ChangeListener(['props1'])
   testProps1Changed([props1]: number[]) {
-    this.props1 = props1;
+    this.props1 = props1
   }
 }
-const callNgOnChanges = (host: any, changes: SimpleChanges) => host.ngOnChanges(changes);
+const callNgOnChanges = (host: any, changes: SimpleChanges) => host.ngOnChanges(changes)
 
 describe('ChangeListener', () => {
   it('works with one input if something change', () => {
-    const test = new Test();
-    expect('ngOnChanges' in test).toBeTruthy();
+    const test = new Test()
+    expect('ngOnChanges' in test).toBeTruthy()
     callNgOnChanges(test, {
       props1: {
-        currentValue: 1,
-      } as SimpleChange,
-    });
-    expect(test.props1).toBe(1);
-  });
+        currentValue: 1
+      } as SimpleChange
+    })
+    expect(test.props1).toBe(1)
+  })
   it('not works if nothing change', () => {
-    const test = new Test();
-    expect('ngOnChanges' in test).toBeTruthy();
-    callNgOnChanges(test, {});
-    expect(test.props1).toBe(0);
-    expect(test.props2).toBe(0);
-  });
+    const test = new Test()
+    expect('ngOnChanges' in test).toBeTruthy()
+    callNgOnChanges(test, {})
+    expect(test.props1).toBe(0)
+    expect(test.props2).toBe(0)
+  })
   it('works with multiple inputs nothing change', () => {
-    const test = new Test();
-    expect('ngOnChanges' in test).toBeTruthy();
+    const test = new Test()
+    expect('ngOnChanges' in test).toBeTruthy()
     callNgOnChanges(test, {
       props1: {
-        currentValue: 1,
+        currentValue: 1
       } as SimpleChange,
       props2: {
-        currentValue: 2,
+        currentValue: 2
+      } as SimpleChange
+    })
+    expect(test.props1).toBe(1)
+    expect(test.props2).toBe(2)
+  })
+  it('will pass ListenerData if is a type of the parameters', () => {
+    const test = new Test()
+    expect('ngOnChanges' in test).toBeTruthy()
+    const changes = {
+      props1: {
+        currentValue: 1
       } as SimpleChange,
-    });
-    expect(test.props1).toBe(1);
-    expect(test.props2).toBe(2);
-  });
+      props2: {
+        currentValue: 2
+      } as SimpleChange
+    }
+    callNgOnChanges(test, changes)
+    expect(test.changes).toBe(changes)
+  })
+  it('if no array of inputs, pass no array but single value', () => {
+    const test = new Test()
+    expect('ngOnChanges' in test).toBeTruthy()
+    callNgOnChanges(test, {
+      props5: {
+        currentValue: 5
+      } as SimpleChange
+    })
+    expect(test.props5).toBe(5)
+  })
   it('works with emitter', () => {
-    const test = new Test();
-    expect('ngOnChanges' in test).toBeTruthy();
+    const test = new Test()
+    expect('ngOnChanges' in test).toBeTruthy()
     callNgOnChanges(test, {
       props3: {
-        currentValue: 1,
+        currentValue: 1
       } as SimpleChange,
       props4: {
-        currentValue: 2,
-      } as SimpleChange,
-    });
-    expect(test.props3).toBe(1);
-    expect(test.props4).toBe(2);
-  });
+        currentValue: 2
+      } as SimpleChange
+    })
+    expect(test.props3).toBe(1)
+    expect(test.props4).toBe(2)
+  })
   it('execute the original ngOnChanges', () => {
-    const test = new TestWithNgOnChanges();
-    expect('ngOnChanges' in test).toBeTruthy();
+    const test = new TestWithNgOnChanges()
+    expect('ngOnChanges' in test).toBeTruthy()
     callNgOnChanges(test, {
       props1: {
-        currentValue: 1,
-      } as SimpleChange,
-    });
-    expect(test.props0).toBe(1);
-    expect(test.props1).toBe(1);
-  });
-});
+        currentValue: 1
+      } as SimpleChange
+    })
+    expect(test.props0).toBe(1)
+    expect(test.props1).toBe(1)
+  })
+})
