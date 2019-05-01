c37c654e9bf3dffda0c4de58e1f5b5d753cb81ec
alfredo.salzillo <noctisbels@gmai.com>
2019-05-01 11:52:09 +0200

    Enhance readme and code

--- README.md
+++ README.md
@@ -1,6 +1,79 @@
-# ng-changes-library
+# ng-changes-listeners
 [![Build Status](https://travis-ci.org/alfredosalzillo/ng-changes-listener.svg?branch=master)](https://travis-ci.org/alfredosalzillo/ng-changes-listener)
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
 
-Add NgOnChanges listener using method and property annotation.
+Add NgOnChanges listener using method and property decorator.
 
+## Why?
+
+As today to listen to changes why need to implement `ngOnChanges`
+or create setter and private variables method and lines of code
+for every `@Input()` we wont to do stuff on changes.
+
+Without **ng-changes-listeners**:
+```typescript
+  import { Component, OnChanges, SimpleChanges, Input } from '@angular/core';
+  
+  @Component({
+    selector: 'my-fancy-component',
+    template: `
+      <div>Awesome</div>
+    `,
+    styles: [],
+  })
+  class MyFancyComponent implements OnChanges {
+    @Input() props1: number;
+    @Input() props2: number;
+    
+    ngOnChanges(changes: SimpleChanges) {
+      if ('props1' in changes) {
+        console.log(`props1 changed: ${changes['props1'].currentValue}`);
+      }
+      if ('props2' in changes) {
+        console.log(`props2 changed: ${changes['props2'].currentValue}`);
+      }
+      if ('props1' in changes && 'props2' in changes) {
+        console.log(`props1 and props2 changed: ${changes['props1'].currentValue} ${changes['props2'].currentValue}`);
+      }
+    }
+  }
+```
+
+With **ng-changes-listeners**:
+```typescript
+  import { Component, Input } from '@angular/core';
+  import { ChangeListener } from 'ng-changes-listeners';
+  
+  @Component({
+    selector: 'my-fancy-component',
+    template: `
+      <div>Awesome</div>
+    `,
+    styles: [],
+  })
+  class MyFancyComponent {
+    @Input() props1: number;
+    @Input() props2: number;
+    
+    @ChangeListener('props1') props1Changed(props1: number) {
+      console.log(`props1 changed: ${props1}`);
+    }
+    
+    @ChangeListener('props2') props1Changed(props2: number) {
+      console.log(`props2 changed: ${props2}`);
+    }
+    
+    @ChangeListener(['props1', 'props2']) props1Changed([props1, props2]) {
+      console.log(`props1 and props2 changed: ${props1} ${props2}`);
+    }
+  }
+```
+
+## Install
+
+Install via npm or yarn.
+
+```bash
+  npm install --save ng-changes-listener
+  yarn add ng-changes-listener
+```
--- src/ng-changes-listener.ts
+++ src/ng-changes-listener.ts
@@ -1,83 +1,91 @@
-import { SimpleChanges, EventEmitter } from '@angular/core'
-import 'reflect-metadata'
+import { SimpleChanges, EventEmitter } from '@angular/core';
+import 'reflect-metadata';
 
-const isEventEmitter = <T = any>(value: any): value is EventEmitter<T> => 'emit' in value
-
-const changesListenersKey = Symbol('changes-listeners')
-const getChangesListeners = (target: Object) =>
-  Reflect.getMetadata(changesListenersKey, target) as Function[]
-const defineChangesListeners = (target: Object) =>
-  Reflect.defineMetadata(changesListenersKey, [], target)
-const pushChangesListeners = (target: Object, value: Function) =>
-  getChangesListeners(target).push(value)
-
-export type ChangeListenerFn = (this: Object, change: SimpleChanges) => void
-export type ChangesPredicate = (changes: SimpleChanges) => boolean
+export type ChangeListenerFn = (this: Object, change: SimpleChanges) => void;
+export type ChangesPredicate = (changes: SimpleChanges) => boolean;
 
 export class ListenerData {
   constructor(public changes: SimpleChanges) {}
 }
 
-const ngOnChanges = function(this: Object, changes: SimpleChanges) {
-  getChangesListeners(this).forEach((listener: Function) => listener.call(this, changes))
-}
-const action = (operation: (host: Object, changes: SimpleChanges) => void): ChangeListenerFn =>
-  function(this: object, changes: SimpleChanges) {
-    operation(this, changes)
-  }
+const isEventEmitter = <T = any>(value: any): value is EventEmitter<T> => 'emit' in value;
 
-export const ChangesListener = (
+const changesListenersKey = Symbol('changes-listeners');
+const getChangesListeners = (target: Object) =>
+  Reflect.getMetadata(changesListenersKey, target) as Function[];
+const defineChangesListeners = (target: Object) =>
+  Reflect.defineMetadata(changesListenersKey, [], target);
+const pushChangesListeners = (target: Object, value: Function) =>
+  getChangesListeners(target).push(value);
+
+const ngOnChanges = function (this: Object, changes: SimpleChanges) {
+  getChangesListeners(this).forEach((listener: Function) => listener.call(this, changes));
+};
+const action = (operation: (host: Object, changes: SimpleChanges) => void): ChangeListenerFn =>
+  function (this: object, changes: SimpleChanges) {
+    operation(this, changes);
+  };
+
+/**
+ * General Decorator Generator
+ * */
+const NgChangesListener = (
   isChanged: ChangesPredicate,
-  transform: (changes: SimpleChanges) => any
+  transform: (changes: SimpleChanges) => any,
 ): PropertyDecorator => (target: any, propertyKey: string | symbol) => {
   if (!Reflect.hasOwnMetadata(changesListenersKey, target)) {
-    defineChangesListeners(target)
+    defineChangesListeners(target);
     if ('ngOnChanges' in target) {
-      pushChangesListeners(target, target.ngOnChanges)
+      pushChangesListeners(target, target.ngOnChanges);
     }
     Object.defineProperty(target, 'ngOnChanges', {
-      value: ngOnChanges
-    })
+      value: ngOnChanges,
+    });
   }
-  const Type = Reflect.getMetadata('design:type', target, propertyKey)
+  const Type = Reflect.getMetadata('design:type', target, propertyKey);
   pushChangesListeners(
     target,
     action((host, changes) => {
-      const listener = Reflect.get(host, propertyKey)
+      const listener = Reflect.get(host, propertyKey);
       if (!isChanged(changes)) {
-        return
+        return;
       }
-      // cannot use only Type === EventEmitter for a know bug
+      // can't use only Type === EventEmitter for a know bug
       // https://github.com/Microsoft/TypeScript/issues/18995
       if (Type === EventEmitter || isEventEmitter(listener)) {
-        return listener.emit(transform(changes))
+        return listener.emit(transform(changes));
       }
       if (Type === Function) {
-        const parametersType: any[] = Reflect.getMetadata('design:paramtypes', target, propertyKey)
+        const parametersType: any[] = Reflect.getMetadata('design:paramtypes', target, propertyKey);
         return listener.call(
           host,
-          ...parametersType.map(paramType => {
+          ...parametersType.map((paramType) => {
             if (paramType === ListenerData) {
-              return new ListenerData(changes)
+              return new ListenerData(changes);
             }
-            return transform(changes)
-          })
-        )
+            return transform(changes);
+          }),
+        );
       }
-    })
-  )
-}
+    }),
+  );
+};
 
-const flatten = <T>(arr: T[] | T) => ([] as T[]).concat(arr)
+const flatten = <T>(arr: T[] | T) => ([] as T[]).concat(arr);
 const everyChanged = <C, S = keyof C>(inputs: S[]) => (changes: SimpleChanges) =>
-  inputs.every(input => typeof input === 'string' && input in changes)
+  inputs.every(input => typeof input === 'string' && input in changes);
 const currentValues = <C, S = keyof C>(inputs: S[]) => (changes: SimpleChanges) =>
-  inputs.map(input => typeof input === 'string' && changes[input] && changes[input].currentValue)
+  inputs.map(input => typeof input === 'string' && changes[input] && changes[input].currentValue);
 const singleValue = <C, S = keyof C>(input: S) => (changes: SimpleChanges) =>
-  typeof input === 'string' && changes[input] && changes[input].currentValue
+  typeof input === 'string' && changes[input] && changes[input].currentValue;
 
+/**
+ * ChangeListener decorator Generator
+ * add the method or the EventEmitter as listener to ngOnChanges lifecycle
+ * @param inputs the properties or array of properties how trigger the changes
+ * */
 export const ChangeListener = <C, S = keyof C>(inputs: S | S[]) =>
-  ChangesListener(
+  NgChangesListener(
     everyChanged<C, S>(flatten<S>(inputs)),
-    typeof inputs === 'string' ? singleValue(inputs) : currentValues<C, S>(flatten<S>(inputs))
-  )
+    typeof inputs === 'string' ? singleValue(inputs) : currentValues<C, S>(flatten<S>(inputs)),
+  );
